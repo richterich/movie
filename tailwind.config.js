edb5829b6c29b427ba9587a472d612b6bbec527d
Alexander Richterich <alexander@richterich.dev>
2023-09-07 00:24:27 +0300

    change tailwindcss config
    Extend default fontFamily with font Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import {fontFamily} from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ['./**/*.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Inter'", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
