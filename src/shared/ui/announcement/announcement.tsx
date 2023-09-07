0da3cefcc5f85bd8e862ad2d91f2787c1663c0ad
Alexander Richterich <alexander@richterich.dev>
2023-09-07 18:21:06 +0300

    merge default and external classes

--- src/shared/ui/announcement/announcement.tsx
+++ src/shared/ui/announcement/announcement.tsx
@@ -1,14 +1,22 @@
 import {type ReactNode, type LegacyRef, forwardRef} from 'react';
+import {cm} from '~/shared/lib';
 
 interface Props {
+  className?: string;
   children: ReactNode;
 }
 
-export const Announcement = forwardRef(function Announcement({children}: Props, ref: LegacyRef<HTMLDivElement>) {
+export const Announcement = forwardRef(function Announcement(
+  {className, children}: Props,
+  ref: LegacyRef<HTMLDivElement>
+) {
   return (
     <div
       ref={ref}
-      className="w-full bg-indigo-500 px-2 py-1 text-center text-base font-semibold text-white transition-transform duration-75">
+      className={cm(
+        'w-full bg-indigo-500 px-4 py-1 text-center text-sm leading-6 text-white transition-transform duration-75 sm:px-6 lg:px-8',
+        className
+      )}>
       {children}
     </div>
   );
--- src/shared/ui/buttons/icon-button.tsx
+++ src/shared/ui/buttons/icon-button.tsx
@@ -1,16 +1,21 @@
 import {type MouseEventHandler} from 'react';
+import {cm} from '~/shared/lib';
 
 interface Props {
+  className?: string;
   text: string;
   icon: string;
   handler: MouseEventHandler;
 }
 
-export const IconButton = ({text, icon, handler}: Props) => {
+export const IconButton = ({className, text, icon, handler}: Props) => {
   return (
-    <button type="button" className="-mx-1 -my-1 flex h-8 w-8 items-center justify-center" onClick={handler}>
+    <button
+      type="button"
+      className={cm('-mx-1 -my-1 flex h-8 w-8 items-center justify-center', className)}
+      onClick={handler}>
       <span className="sr-only">{text}</span>
-      <span aria-hidden="true" className="material-symbols-outlined">
+      <span aria-hidden="true" className="material-symbols-outlined select-none">
         {icon}
       </span>
     </button>
