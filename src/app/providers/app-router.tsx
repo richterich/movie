1b44e870c4025e3b03a1d4d51eb7857afe2d15a3
Alexander Richterich <alexander@richterich.dev>
2023-09-08 19:04:43 +0300

    use LoadSpinner in AppRouter

--- src/app/providers/app-router.tsx
+++ src/app/providers/app-router.tsx
@@ -1,10 +1,11 @@
 import type {Router} from '@remix-run/router';
 import {Suspense} from 'react';
 import {RouterProvider} from 'react-router-dom';
+import {LoadSpinner} from '~/widgets/loader';
 
 export const AppRouter = (routing: Router) => {
   const Page = () => (
-    <Suspense fallback="Loading...">
+    <Suspense fallback={<LoadSpinner className="h-screen" />}>
       <RouterProvider router={routing} />
     </Suspense>
   );
