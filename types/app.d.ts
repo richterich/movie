cfe20fd7da7637048ece2229e1ed2f943076e131
Alexander Richterich <alexander@richterich.dev>
2023-10-30 19:04:54 +0200

    update app types
    Add Id type, and swiper html element JSX type

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,7 +1,22 @@
+import type {SwiperContainer, SwiperSlide} from 'swiper/element';
+
 declare global {
+  type Id = number | null | undefined;
+
   declare namespace App {
     declare type AppDispatch = import('~/app/providers/app-store.config').AppDispatch;
     declare type RootState = import('~/app/providers/app-store.config').RootState;
+  }
+
+  namespace JSX {
+    interface IntrinsicElements {
+      'swiper-container': React.DetailedHTMLProps<React.HTMLAttributes<SwiperContainer>, SwiperContainer> & {
+        init: string | boolean;
+      };
+      'swiper-slide': React.DetailedHTMLProps<React.HTMLAttributes<SwiperSlide>, SwiperSlide> & {
+        lazy: string | boolean;
+      };
+    }
   }
 }
 
