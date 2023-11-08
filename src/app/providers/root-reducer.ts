87cd018fa8eb332320a40e8cfa4bd2083c0c4c21
Alexander Richterich <alexander@richterich.dev>
2023-11-08 20:02:08 +0200

    add genre slice to root reducer

--- src/app/providers/root-reducer.ts
+++ src/app/providers/root-reducer.ts
@@ -1,6 +1,8 @@
 import {combineReducers} from '@reduxjs/toolkit';
+import {genreSlice} from '~/entities/genre';
 import {baseApi} from '~/shared/api';
 
 export const reducer = combineReducers({
   [baseApi.reducerPath]: baseApi.reducer,
+  [genreSlice.name]: genreSlice.reducer,
 });
