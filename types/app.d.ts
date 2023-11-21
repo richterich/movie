89fa2616afd3fcde2a3b10e396f973f1ac028907
Alexander Richterich <alexander@richterich.dev>
2023-11-21 14:19:46 +0200

    update global types
    Add global generic types: ValueOf, KeyOf, MapOf, KeyedMapOf.

--- types/app.d.ts
+++ types/app.d.ts
@@ -3,6 +3,18 @@ import type {SwiperContainer, SwiperSlide} from 'swiper/element';
 declare global {
   type Id = number | null | undefined;
 
+  type ValueOf<T> = T[keyof T];
+
+  type KeyOf<T> = keyof T;
+
+  type MapOf<V> = {
+    [key: string | number]: V;
+  };
+
+  type KeyedMapOf<K, V> = {
+    [Property in keyof K]: V;
+  };
+
   declare namespace App {
     declare type AppDispatch = import('~/app/providers/app-store.config').AppDispatch;
     declare type RootState = import('~/app/providers/app-store.config').RootState;
