d70b29f8cec0d9407a207160e442dacdae1d9a75
Alexander Richterich <alexander@richterich.dev>
2023-11-22 15:26:13 +0200

    refactor overlay component
    add right padding to the body to prevent annoying twitching

--- src/shared/ui/overlay/overlay.tsx
+++ src/shared/ui/overlay/overlay.tsx
@@ -1,15 +1,22 @@
-import {type PropsWithChildren, type HTMLAttributes, useEffect} from 'react';
+import type {HTMLAttributes} from 'react';
+import {useEffect} from 'react';
 import {cm} from '~/shared/lib';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {}
 
-export const Overlay = ({className, children}: PropsWithChildren<Props>) => {
+export const Overlay = ({className, children}: Props) => {
   useEffect(() => {
+    const originOverflow = document.body.style.overflow;
+    const scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+
+    document.body.style.paddingRight = `${scrollBarWidth}px`;
     document.body.style.overflow = 'hidden';
+
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = originOverflow;
+      document.body.style.paddingRight = '0px';
     };
   }, []);
 
-  return <div className={cm('fixed inset-0 z-50 select-none bg-black/95 backdrop-blur-sm', className)}>{children}</div>;
+  return <div className={cm('fixed inset-0 z-50 select-none bg-black/90 backdrop-blur-sm', className)}>{children}</div>;
 };
