dd8eb15a0aac85375aee75a768f121d981ce0540
Alexander Richterich <alexander@richterich.dev>
2023-11-23 18:09:07 +0200

    add redux slice for Watch Videos feature

--- src/app/providers/root-reducer.ts
+++ src/app/providers/root-reducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from '@reduxjs/toolkit';
+import {watchVideosSlice} from '~/features/watch-videos';
 import {genreSlice} from '~/entities/genre';
 import {baseApi} from '~/shared/api';
 
 export const reducer = combineReducers({
   [baseApi.reducerPath]: baseApi.reducer,
   [genreSlice.name]: genreSlice.reducer,
+  [watchVideosSlice.name]: watchVideosSlice.reducer,
 });
