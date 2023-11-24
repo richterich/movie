2cbb0389d513db129fa374dee01f5cde3a554dac
Alexander Richterich <alexander@richterich.dev>
2023-11-24 14:41:10 +0200

    refactor load spinner widget
    Add custom stroke width

--- src/widgets/loader/load-spinner.tsx
+++ src/widgets/loader/load-spinner.tsx
@@ -3,13 +3,14 @@ import {SpinnerCircle} from '~/shared/ui/spinner';
 
 interface Props {
   hexColor?: string;
+  strokeWidth?: string | number;
   className?: string;
 }
 
-export const LoadSpinner = ({hexColor, className}: Props) => {
+export const LoadSpinner = ({hexColor, strokeWidth, className}: Props) => {
   return (
     <div className={cm('flex items-center justify-center', className)}>
-      <SpinnerCircle stroke={hexColor || '#fff'} />
+      <SpinnerCircle stroke={hexColor || '#fff'} strokeWidth={strokeWidth || '4px'} />
     </div>
   );
 };
