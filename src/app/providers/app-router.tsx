67ef97e59a231e6b6f237abee30454311c531c1c
Alexander Richterich <alexander@richterich.dev>
2023-11-24 14:45:00 +0200

    refactor router provider
    Change load spinner color & size

--- src/app/providers/app-router.tsx
+++ src/app/providers/app-router.tsx
@@ -3,6 +3,11 @@ import {LoadSpinner} from '~/widgets/loader';
 import {router} from './app-router.config';
 
 export const AppRouter = () => {
-  const Router = () => <RouterProvider router={router} fallbackElement={<LoadSpinner className="h-screen" />} />;
+  const Router = () => (
+    <RouterProvider
+      router={router}
+      fallbackElement={<LoadSpinner strokeWidth={'8px'} hexColor="#86198f" className="h-screen" />}
+    />
+  );
   return Router;
 };
