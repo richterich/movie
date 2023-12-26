ef7a87ef57df48c8a71c9134bf5c74b385b21364
Alexander Richterich <alexander@richterich.dev>
2023-12-26 20:10:02 +0200

    update jest config
    Specify path's aliases in jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,19 @@
 import type {JestConfigWithTsJest} from 'ts-jest';
+import {pathsToModuleNameMapper} from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(
+    {
+      '~': ['src'],
+      '~/*': ['src/*'],
+    },
+    {
+      prefix: '<rootDir>/',
+      useESM: true,
+    }
+  ),
 };
 
 export default config;
